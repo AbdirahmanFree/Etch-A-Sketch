43dbf9cb4a65c83c8e5e7a74c92658c5d9254a74
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-07-20 00:07:20 -0700

    Added the grid creating logic lowkey asked chatgpt

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const container = document.querySelector(".container");
-for(let i = 0; i<576; i++){
+ const squareSize = 100 / 16;
+for(let i = 0; i<256; i++){
     let square = document.createElement("div")
+    square.addEventListener("mouseover",function(){
+        square.style.backgroundColor = "black";
+    })
+    square.style.width = `${squareSize}%`;
+    square.style.height = `${squareSize}%`;
     square.setAttribute("id",`square_${i+1}`);
     container.appendChild(square);
 }
